881be799d3cff3014aa7e764cc6d9ba1c1686f77
stasy <nastexon@mail.ru>
2023-01-03 09:59:14 +0300

    seminar 6

---
+++ задача41/Program.cs
@@ -0,0 +1,76 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+/*
+1. Проверка на ввод числа
+2. ввод чисел с клавиатуры
+3. подсчет положительных
+*/
+
+//поверка на число
+
+int GetNumber(string message)
+{
+    int result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//метод инициализации массива
+
+int[] InitArray(int dimention)
+{
+    int[] array = new int [dimention];
+    Random rnd = new Random();
+    for (int i = 0; i < dimention; i++)
+    {
+        array[i] = GetNumber ("Введите элемент массива");;
+    }
+    return array;
+}
+
+//метод печати массива
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// метод подсчёта количества элементов, удовлетворяющих условию
+
+int CountNumbers (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+        count ++;
+        }
+    }
+    return count;
+}
+
+int dim = GetNumber ("Введите размерность массива");
+int [] array = InitArray(dim);
+PrintArray(array);
+Console.WriteLine($"Количество положительных элементов {CountNumbers(array)}");
---
+++ задача46/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+/*
+1. Проверка на ввод числа
+2. ввод чисел с клавиатуры
+3. решение уравнения
+*/
+
+//поверка на число
+
+double GetNumber(string message)
+{
+    double result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//введение коэффициентов
+
+double k1 = GetNumber ("Enter first k");
+double k2 = GetNumber ("Enter second k");
+double b1 = GetNumber ("Enter first b");
+double b2 = GetNumber ("Enter second b");
+
+// решение уравнения. y = k1 * x + b1, y = k2 * x + b2
+
+double x = ((b2-b1)/(k1-k2));
+Console.WriteLine($"Координата х пересечения прямых равна {x}");
+
+double y = ((k1*x)+b1);
+Console.WriteLine($"Координата у пересечения прямых равна {y}");
+
