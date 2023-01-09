a42234babca91fb534a4c5a770dcf38fa1e5dcaf
stasy <nastexon@mail.ru>
2023-01-09 14:51:56 +0300

    seminar 7

---
+++ задача50/Program.cs
@@ -0,0 +1,81 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет
+*/
+
+
+int GetNumber(string message)
+{
+    int result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите кол-во строк");
+int n = GetNumber("Введите кол-во столбцов");
+
+int[,] matrix = InitMatrix(m,n);
+
+PrintArray(matrix);
+
+
+int x = GetNumber("Введите позицию элемента");
+int i = x/10;
+int j = x%10;
+
+if (i >= m || j >= n)
+    Console.WriteLine($"Элемента не существует");
+else 
+    Console.WriteLine($"Искомый элемент матрицы {matrix[i,j]}");
+
+
+
+
